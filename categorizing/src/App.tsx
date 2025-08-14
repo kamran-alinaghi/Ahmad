26c60e3d66aa257e6905998ce41e93b87f83d2c5
kamran-alinaghi <kamran.alinaghi.canada@gmail.com>
2025-08-13 19:06:06 -0700

    reload auth fix

--- categorizing/src/App.tsx
+++ categorizing/src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/Login/Login';
 import SignupPage from './components/SignUp/SignupPage';
 import ProjectSelection from './components/ProjectSelection/ProjectSelection';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import type { RootState } from './redux/store';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './utils/firebaseConfig';
+import { logout, setUser } from './redux/userSlice';
 
 const App: React.FC = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userInAuth) => {
+      if (userInAuth) {
+        dispatch(setUser({
+          email: userInAuth.email!,
+          name: userInAuth.displayName,
+          photoURL: userInAuth.photoURL,
+        }));
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
+  
   const user = useSelector((state: RootState) => state.user);
 
   return (

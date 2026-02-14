e3390f997a997beb4b11ccd8254fb79cb9081a86
kamran-alinaghi <kamran.alinaghi.canada@gmail.com>
2026-02-14 14:20:03 -0800

    Reset results on table change

--- categorizing/src/components/TableContainer/TableContainer.tsx
+++ categorizing/src/components/TableContainer/TableContainer.tsx
@@ -26,6 +26,7 @@ const TableContainer = () => {
     setLocalTable(table || { columns: [], rows: [] });
     setTempTable(table || { columns: [], rows: [] });
     setIsDirty(false);
+    setResult(null);
   }, [table]);
 
   const handleSave = () => {
